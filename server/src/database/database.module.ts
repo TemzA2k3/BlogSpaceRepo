f36e41862f9bebf3a538d01f97738f6676ff6797
Prikluchenie2003 <kikotik91@gmail.com>
2026-01-03 18:41:39 +0200

    fix database conf

--- server/src/database/database.module.ts
+++ server/src/database/database.module.ts
@@ -17,7 +17,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
                 database: config.get<string>('DB_NAME'),
                 autoLoadEntities: true,
                 // synchronize только в dev, НИКОГДА в prod!
-                synchronize: config.get<string>('NODE_ENV') !== 'production',
+                synchronize: true,// config.get<string>('NODE_ENV') !== 'production',
                 // Логирование только в dev
                 logging: config.get<string>('NODE_ENV') !== 'production',
             }),
